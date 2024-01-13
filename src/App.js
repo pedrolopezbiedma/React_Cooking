953faa0117e2bee7a419e7b43b0f1284f52f570c
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-13 16:29:46 +0100

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Recipe from "./pages/Recipe";
+import Search from "./pages/Search";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
