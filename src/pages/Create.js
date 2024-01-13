20afe99f7381ed3b36151e4f7c85af54e0731d35
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-13 22:53:25 +0100

    Post to create a new recipe

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,9 @@
 // Styles
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+// Styles
 import "./Create.css";
 
 const Create = () => {
@@ -8,6 +12,11 @@ const Create = () => {
   const [method, setMethod] = useState("");
   const [ingredient, setIngredient] = useState("");
   const [recipeIngredients, setRecipeIngredients] = useState([]);
+  const { postData, data: result } = useFetch(
+    "http://localhost:3005/recipes",
+    "POST"
+  );
+  const history = useHistory();
 
   const onAddIngredient = (event) => {
     event.preventDefault();
@@ -18,16 +27,18 @@ const Create = () => {
       setIngredient("");
     }
   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(
-      "Values in the form are -->",
-      title,
-      cookingTime,
-      recipeIngredients,
-      method
-    );
+    postData({ title, cookingTime, ingredients: recipeIngredients, method });
   };
+
+  useEffect(() => {
+    if (result) {
+      history.push("/");
+    }
+  }, [result, history]);
+
   return (
     <div className="create">
       <h2 className="page-title">Add a new Recipe</h2>
@@ -59,7 +70,7 @@ const Create = () => {
               onChange={(event) => setIngredient(event.target.value)}
             />
             <button className="btn" onClick={onAddIngredient}>
-              Add Ingredient
+              Add
             </button>
           </div>
           <p className="ingredients">
