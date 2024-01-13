7f20b35df8c386a1e416d4aeab280970ac9479a4
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-13 17:56:27 +0100

    Create the form to a new recipe

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,50 @@
+// Styles
+import { useState } from "react";
+import "./Create.css";
+
 const Create = () => {
-  return <h1>This is the create component</h1>;
+  const [title, setTitle] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [method, setMethod] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a new Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Title</span>
+          <input
+            type="string"
+            required
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>Cooking Time</span>
+          <input
+            type="number"
+            required
+            value={cookingTime}
+            onChange={(event) => setCookingTime(event.target.value)}
+          />
+        </label>
+        {/* Ingredients  */}
+        <label>
+          <span>Method</span>
+          <textarea
+            typeof="string"
+            required
+            value={method}
+            onChange={(event) => setMethod(event.target.value)}
+          />
+        </label>
+        <button className="btn">Create</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
