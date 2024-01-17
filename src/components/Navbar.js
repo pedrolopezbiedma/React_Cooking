3aaa11eebe12f16cf81a4e22b9a763c7d4b95ca8
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-17 08:27:57 +0100

    Use the context in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
+// React
 import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
+
 // Styles
 import "./Navbar.css";
 
+// Hooks
+import { useThemeContext } from "../hooks/useThemeContext";
+
+// Components
+import SearchBar from "./SearchBar";
+
 const Navbar = () => {
+  const { color } = useThemeContext();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Site</h1>
