023c434ca901c6bb6a9f1cf9138429c4c9e72c16
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-17 22:24:15 +0100

    Delete the recipe from firebase

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,27 @@
 // React
 import { Link } from "react-router-dom";
 
+// Firebase
+import { projectFirestore } from "../firebase/config";
+
 // Context
 import { useThemeContext } from "../hooks/useThemeContext";
 
 // Styles
+import TrashCan from "../assets/trashcan.svg";
 import "./RecipeList.css";
 
 const RecipeList = ({ recipes }) => {
   const { mode } = useThemeContext();
+
+  const handleDelete = async (recipeId) => {
+    try {
+      await projectFirestore.collection("recipes").doc(recipeId).delete();
+    } catch (error) {
+      console.log("Error when deleting a recipe", error);
+    }
+  };
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -17,6 +30,12 @@ const RecipeList = ({ recipes }) => {
           <p>{recipe.cookingTime} to make</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
+          <img
+            onClick={() => handleDelete(recipe.id)}
+            className="delete"
+            src={TrashCan}
+            alt="Delete recipe"
+          />
         </div>
       ))}
     </div>
