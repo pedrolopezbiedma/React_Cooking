c409f23e28e49041217a64c0d65a78bf9157a180
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-17 21:42:31 +0100

    Fetch all recipes from firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,45 @@
+// React
+import { useState, useEffect } from "react";
+
+// Firebase
+import { projectFirestore } from "../firebase/config";
+
+// Component
 import RecipeList from "../components/RecipeList";
-import { useFetch } from "../hooks/useFetch";
+// import { useFetch } from "../hooks/useFetch";
 
 // Styles
 import "./Home.css";
 
 const Home = () => {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch("http://localhost:3005/recipes");
+  const [recipes, setRecipes] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    const unsub = projectFirestore.collection("recipes").onSnapshot(
+      (snapshot) => {
+        let recipes = [];
+        snapshot.docs.forEach((doc) => {
+          const recipe = doc.data();
+          recipes.push({ id: doc.id, ...recipe });
+        });
+        setRecipes(recipes);
+        setIsPending(false);
+      },
+      (error) => {
+        setIsPending(false);
+        setError(error.message);
+      }
+    );
+
+    // Cleanup
+    return () => {
+      unsub();
+    };
+  }, []);
 
   return (
     <div className="home">
